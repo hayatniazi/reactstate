49e4da31e2c3cb1979d1685d6cfb9c6be5308459
hayatniazi <66178207+hayatniazi@users.noreply.github.com>
2020-06-10 05:41:35 +0100

    React state

---
+++ src/Room.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function Room() {
+    let [isLit, setLit] = useState(true);
+    let [getAge, setAge] = useState(25);
+    let [happy, setmode] = useState(true);
+    function update(){
+        
+        console.log("Button pressed");
+        setLit(!isLit);
+        setmode(!happy);
+        console.log('line executed');
+    }
+    const updateAge=()=>{
+        console.log("Update Age");
+        setAge(++getAge);
+    }
+
+    const decAge=()=>{
+        console.log("Decrease Age");
+        setAge(--getAge);
+    }
+    return (
+    <div>
+        Huma Hayat love lit room, but light colors so now it's {isLit ? 'Lit' : 'Dark'} and she is { happy ? "Happy" : "Sad"}.
+        <br/>
+        Age : {getAge}
+        <br/>
+        <button onClick={update}>Toggle Light</button>
+    <br/>
+        <button onClick={updateAge}>Increase Age</button>
+        <br/>
+        <button onClick={decAge}>Decrease Age</button>
+        <br/>
+        <button onClick={()=>{
+            console.log('Button tag with arrow function');
+        }}
+        
+        >Arrow function</button>
+    </div>
+);
+}
+export default Room;
