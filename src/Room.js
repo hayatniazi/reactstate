55d90433211fcd5162f23dfc647593ff8e434716
hayatniazi <66178207+hayatniazi@users.noreply.github.com>
2020-06-11 04:20:05 +0100

    added state and some css

--- src/Room.js
+++ src/Room.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
-import './App.css';
-
+import './Room.css';
 function Room() {
     let [isLit, setLit] = useState(true);
     let [getAge, setAge] = useState(25);
@@ -22,22 +21,27 @@ function Room() {
         setAge(--getAge);
     }
     return (
-    <div>
-        Hayat love lit room, but light colors so now it's {isLit ? 'Lit' : 'Dark'} and he is { happy ? "Happy" : "Sad"}.
+    <div className={`room ${isLit ? 'lit':'dark'}`}>
+        <h1>Welcome to my page..!</h1>
+        
+        <hr/>
+        <h4>I created this page using ReactJs with surge deployment using github action. Below buttons are asigned some specific functionalities which you can test by yourself and find out what each button do.</h4>
+        <p>Hayat love lit room, but light colors so now it's {isLit ? "lit" : "dark"} and he is { happy ? "Happy" : "Sad"}. He said his age is : {getAge} but I don't think so, click below button to increase his age you think he is..:D.Take a screenshot and send back to me after increasing my age and setting my room light.</p> 
         <br/>
-        Age : {getAge}
-        <br/>
-        <button onClick={update}>Toggle Light</button>
-    <br/>
-        <button onClick={updateAge}>Increase Age</button>
-        <br/>
-        <button onClick={decAge}>Decrease Age</button>
-        <br/>
-        <button onClick={()=>{
+        <button className='d' onClick={update}>Toggle Light</button>
+    
+        <button className='b' onClick={updateAge}>Increase Age</button>
+        
+        <button className='b' onClick={decAge}>Decrease Age</button>
+        
+        {/* <button className='b' onClick={()=>{
             console.log('Button tag with arrow function');
         }}
         
-        >Arrow function</button>
+        >Arrow function</button> */}
+
+        <h5>Thank you for visiting my page, it is just for fun and learning and I know reading this line is difficult..:p</h5>
+        <p><i>Hayat Muhammad Niazi</i></p>
     </div>
 );
 }
